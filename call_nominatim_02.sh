b5866e36a8c74e9acf7efdaa2f492002c52beb4a
Andy Townsend <gitconfigh20@atownsend.org.uk>
2025-09-14 19:38:49 +0000

    Change nominatim script to tee into setup.log again.

--- call_nominatim_02.sh
+++ call_nominatim_02.sh
@@ -2,4 +2,4 @@ dropdb nominatim
 cd ~/nominatim-project
 export USERNAME=nominatim
 export USERHOME=/srv/nominatim
-. $USERHOME/nominatim-venv/bin/activate;time nominatim import --osm-file /home/ajtown/data/nominatim/transformed_after.pbf --no-updates  2>&1 > setup_$$.log
+. $USERHOME/nominatim-venv/bin/activate;time nominatim import --osm-file /home/ajtown/data/nominatim/transformed_after.pbf --no-updates  2>&1 | tee setup_$$.log
